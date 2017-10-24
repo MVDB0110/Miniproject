6910c11f64ee2b0d880ef21cdbdc0f4f0f56c755
Mike van den Brink <mbrink1999@outlook.com>
2017-10-24 11:19:02 +0200

    Verschillende stations en een check op station invoer

--- gui.py
+++ gui.py
@@ -6,15 +6,34 @@ def tkinter_reis_label():
     stationsnaam = station.get()
     if stationsnaam.lower() in lijst():
         ns_api(stationsnaam)
-        reisinfo = informatie()
-        reislabel = Text(root, width=len(reisinfo.split('\n')[ 0 ]) + 1, height=(reisinfo.count('\n') + 1), background=background, highlightbackground='black')
-        reislabel.pack()
-        reislabel.insert(END, reisinfo)
-        reislabel.config(state=DISABLED)
+        home.pack(side=LEFT)
         welkom.pack_forget()
         knoppen.pack_forget()
+        reislabel.pack()
     else:
-        station.insert(0,"Dit station kan niet worden gevonden in de database.")
+        station.insert(0, "Dit station kan niet worden gevonden in de database.")
+
+def homescreen():
+    welkom.pack(pady=50)
+    knoppen.pack(padx=100, pady=100, side=BOTTOM)
+    ams.pack(side=LEFT)
+    loskaartje.pack(side=LEFT)
+    koopovchip.pack(side=LEFT)
+    buitenland.pack(side=LEFT)
+    reisinfo.pack(side=LEFT)
+    station.pack(side=RIGHT, padx=10, pady=10)
+
+def back():
+    home.pack_forget()
+    reislabel.pack_forget()
+    welkom.pack(pady=50)
+    knoppen.pack(padx=100, pady=100, side=BOTTOM)
+    ams.pack(side=LEFT)
+    loskaartje.pack(side=LEFT)
+    koopovchip.pack(side=LEFT)
+    buitenland.pack(side=LEFT)
+    reisinfo.pack(side=LEFT)
+    station.pack(side=RIGHT, padx=10, pady=10)
 
 root = Tk()
 
@@ -25,19 +44,24 @@ root.configure(bg='yellow')
 background = 'yellow'
 backgroundknop = 'blue'
 
+reisinfo = informatie()
+reislabel = Text(root, width=len(reisinfo.split('\n')[ 0 ]) + 1, height=(reisinfo.count('\n') + 1),background=background, highlightbackground='black')
+reislabel.insert(END, reisinfo)
+reislabel.config(state=DISABLED)
+
 header = Frame(root,bg='blue')
 header.pack(side=TOP, fill=X)
+
+home = Button(header,text='Back',highlightbackground=backgroundknop,command=back)
 
 welkom = Label(master=root,
                text='Welkom bij NS',
                foreground='blue',
                font=('Verdana',50,'bold italic'),
                background=background)
-welkom.pack(pady=50)
 
 station = Entry(header, width=30, bg='white', highlightbackground='blue')
 station.insert(0,'Vul hier een stationnaam in.')
-station.pack(side=RIGHT,padx=10,pady=10)
 
 bottom = Frame(root,bg='blue',)
 bottom.pack(side=BOTTOM,fill=X)
@@ -50,21 +74,17 @@ nedvlag.pack(side=LEFT,padx=10,pady=10)
 
 
 knoppen = Frame(master=root, background=background)
-knoppen.pack(padx=100 ,pady=100, side=BOTTOM)
 
 ams = Button(knoppen, text='Ik wil naar Amsterdam', highlightbackground=background, background='blue')
-ams.pack(side=LEFT)
 
 loskaartje = Button(knoppen, text='Ik wil een los kaartje kopen', highlightbackground=background, background='blue')
-loskaartje.pack(side=LEFT)
 
 koopovchip = Button(knoppen, text='Kopen OV-chipkaart', highlightbackground=background, background='blue')
-koopovchip.pack(side=LEFT)
 
 buitenland = Button(knoppen, text='Ik wil naar het buitenland', highlightbackground=background, background='blue')
-buitenland.pack(side=LEFT)
 
 reisinfo = Button(knoppen, text='Ik wil de reisinformatie van het aangegeven station', highlightbackground=background, background='blue', command=tkinter_reis_label)
-reisinfo.pack(side=LEFT)
+
+homescreen()
 
 root.mainloop()
