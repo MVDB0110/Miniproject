2f57db0a59d74eb7d2db124f7c20ffb6014b2725
Mike van den Brink <mbrink1999@outlook.com>
2017-10-25 20:55:56 +0200

    Inzichtelijk maken wat word gedaan bij elke opdracht/statement

--- ns_api.py
+++ ns_api.py
@@ -8,6 +8,7 @@ def schrijven(reisinformatie):
                 station.write(reisinformatie)
             except:
                 print("Kan bestand niet schrijven.")
+    #Schrijf naar bestand 'vertrektijden.xml'
 
     except:
         print("Kan bestand niet openen.")
@@ -18,22 +19,27 @@ def ns_api(station):
         ns_url = 'http://webservices.ns.nl/ns-api-avt?station='+str(station)
         ns_response = requests.get(ns_url, auth=ns_inlog_gegevens)
         ns_response = xmltodict.unparse(xmltodict.parse(ns_response.text),pretty=True)
+    #Verbinding maken met NS API
+
     except:
         print("Kan niet verbinden met de NS")
 
     schrijven(ns_response)
+    #Stuur informatie naar schrijf functie
 
 def opvragen():
     try:
         with open('vertrektijden.xml', 'r') as station:
             vertrektijden = xmltodict.parse(station.read())
             return vertrektijden
+        #Open bestand 'vertrektijden.xml' en maak het verwerkbaar
     except:
         return "Kan bestand niet openen."
 
 def informatie():
     info = opvragen()
     reisinfo = "{:35}{:35}{:35}{:35}{:35}{:35}\n".format("Bestemming", "Tijd van vertrek", "Vertraging", "Soort trein", "Treinspoor","Reistip")
+    #Variabele creeeren
 
     for trein in info['ActueleVertrekTijden']['VertrekkendeTrein']:
         vertrektijd = trein[ 'VertrekTijd' ].split('T')
@@ -55,7 +61,8 @@ def informatie():
             tip = trein['ReisTip']
         except:
             tip = 'Geen tip bekend'
+        #Informatie verwerken naar gewenste layout
 
         reisinfo +="{:35}{:35}{:35}{:35}{:35}{}\n".format(trein['EindBestemming'], vertrektijd, vertraging, trein['TreinSoort'], vertrekspoor, tip)
-
+        #Informatie in een formatted string zetten
     return reisinfo
--- stationlijst.py
+++ stationlijst.py
@@ -8,17 +8,23 @@ def lijst():
         ns_url = 'http://webservices.ns.nl/ns-api-stations-v2'
         ns_response = requests.get(ns_url, auth=ns_inlog_gegevens)
         ns_response = xmltodict.parse(ns_response.text)
+        #Verbinding maken met NS API
 
+    except:
+        print("Kan niet verbinden met de NS")
+
+    try:
         for station in ns_response['Stations']['Station']:
             if station['Land'] == 'NL':
                 stations.append(station['Namen']['Kort'].lower())
                 stations.append(station['Namen']['Middel'].lower())
                 stations.append(station['Namen']['Lang'].lower())
+        #Voeg station namen toe aan list
 
         stations.sort()
         stations = set(stations)
         stations = list(stations)
+        #Verwijder dubbele namen
         return stations
-
     except:
-        print("Kan niet verbinden met de NS")
+        print("Kan opgevraagde informatie niet verwerken.")
